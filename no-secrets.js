26dffaf5c6a3c5414e6056025dc28bd7f14702f4
Jonathan Galentine <j.galentine@gmail.com>
2018-05-14 14:04:07 -0600

    Squashed commit of the following:
    commit 6375e80389da89227daad9d49b89add01bc1c34a
    Author: Jonathan Galentine <j.galentine@gmail.com>
    Date:   Mon May 14 13:33:45 2018 -0600
    
        Fix JSON loading to support relative paths.
    
    commit 29b6e8e9a2607866d95983fa8a075b4a19e38e63
    Author: Jonathan Galentine <j.galentine@gmail.com>
    Date:   Mon May 14 13:09:29 2018 -0600
    
        Prettify
    
    commit 0b4f1a191edbb721ca9dd8bb1916a4ecc446c127
    Author: Jonathan Galentine <j.galentine@gmail.com>
    Date:   Mon May 14 12:39:08 2018 -0600
    
        Move json schema to its own file.
    
    commit b181497fd98320285a0ee5775bba8ae6938ab254
    Author: Jonathan Galentine <j.galentine@gmail.com>
    Date:   Mon May 14 11:42:52 2018 -0600
    
        Refactored code to remove globals, add schema checking.

--- no-secrets.js
+++ no-secrets.js
@@ -1,33 +1,56 @@
+"use strict";
 const fs = require('fs');
+const Ajv = require('ajv');
 
+/**
+ * Filter mode options.
+ */
 const FilterMode = {
     SMUDGE: 'smudge',
     CLEAN: 'clean'
 };
 
+/**
+ * Escapes a single '$' as double '$$' in an input string for regex replacement.
+ * @param {string} input Input string.
+ */
 function escapeFilter(input) {
     input =  input.replace('$', '$$$$');
     return input
 };
 
-function getFilterValue(filter) {
-    if (filterMode == FilterMode.SMUDGE)
+/**
+ * Returns the filter mode in a given configuration.
+ * @param {*} config no-secrets configuration object.
+ * @param {*} filter 
+ */
+function getFilterValue(config, filter) {
+    if (config.filterMode == FilterMode.SMUDGE)
         return escapeFilter(filter.smudge);
     else
         return escapeFilter(filter.clean);
 };
 
-function filter(data) {
+/**
+ * Filters a data string.
+ * @param {*} config no-secrets configuration object.
+ * @param {string} data Data string to filter.
+ */
+function filter(config, data) {
     let line = data.toString();
     
     for (var filter of config.filters) {
         const regexp = new RegExp(filter.regex);
-        line = line.replace(regexp, '$1' + getFilterValue(filter) + '$3' );
+        line = line.replace(regexp, '$1' + getFilterValue(config, filter) + '$3' );
     }
 
     process.stdout.write(line);
 };
 
+/**
+ * Returns the chosen filter mode command line argument.
+ * @throws Will throw if argument is missing or invalid.
+ */
 function getArgvFilterMode() {
     let arg = process.argv[3];
 
@@ -37,22 +60,58 @@ function getArgvFilterMode() {
         case 'clean':
             return FilterMode.CLEAN;
         default: 
-            console.log('Please specify the filter mode (smudge|clean). For example: node no-secrets.js ./no-secrets.json clean');
-            process.exit(1);
+            throw 'Please specify the filter mode (smudge|clean). For example: node no-secrets.js ./no-secrets.json clean';
     }
 };
 
+/**
+ * Returns the chosen configuration file command line argument.
+ * @throws Will throw if configuration file argument is missing or file does not exist.
+ */
 function getArgvConfigFile() {
     let arg = process.argv[2];
 
-    if (arg == null || !fs.existsSync(arg)) {
-        console.log('Please specify the configuration relative or absolute path. For example: node no-secrets.js ./no-secrets.json clean');
-        process.exit(1);
-    }
+    if (arg == null)
+        throw 'Please specify the configuration relative or absolute path. For example: node no-secrets.js ./no-secrets.json clean';
+    else if (!fs.existsSync(arg))
+        throw 'Configuration file not found.'
     else
-        return arg;
-}
+        return arg
+};
 
-const config = require(getArgvConfigFile());
-const filterMode = getArgvFilterMode();
-process.stdin.on("data", filter);  
+/**
+ * Validates a no-secrets configuration object using JSON schema.
+ * @param {*} configSchema no-secretes configuration schema object.
+ * @param {*} config no-secrets configuration object.
+ * @throws Will throw if configuration is invalid
+ */
+function validateConfig(configSchema, config) {
+    let ajv = new Ajv({allErrors: true, schemas: [configSchema]});
+    let valid = ajv.validate(configSchema, config);
+    if (!valid)
+        throw 'Error(s) occured reading the configuration file:\n' + ajv.errors.map(e => e.message).join('\n');
+};
+
+/**
+ * Read a JSON file using 'fs'.
+ * @param {fs} fs File system module.
+ * @param {string} path File path.
+ */
+function readJsonFile(fs, path) {
+    let data = fs.readFileSync(path);
+    return JSON.parse(data);
+};
+
+try {
+    const configSchema = require('./configSchema.json');
+    let config = readJsonFile(fs, getArgvConfigFile());
+
+    validateConfig(configSchema, config);
+    config.filterMode = getArgvFilterMode();
+
+    process.stdin.on("data", function(data) { filter(config, data) });
+}
+catch(err) {
+    console.error(err);
+    process.exit(1);
+}
